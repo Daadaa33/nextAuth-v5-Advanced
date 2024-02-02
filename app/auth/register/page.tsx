901f28668be907fc0267995b02ab65ee273087b8
exrozza <exrozza@gmail.com>
2024-02-02 20:35:28 +0300

    test 1

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "@/components/auth/RegisterForm";
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
---
+++ lib/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+    var prisma : PrismaClient | undefined;
+}
+
+let db: PrismaClient;
+
+// check if PrismaClient is not on `globalThis` object
+if (process.env.NODE_ENV === 'production') {
+    db = new PrismaClient();
+} else {
+    if (!globalThis.prisma) {
+        globalThis.prisma = new PrismaClient();
+    }
+    db = globalThis.prisma;
+}
+
+export default db;
